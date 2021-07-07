7519a83a2aea5308ffd36cfc13752a9774e3f4df
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-07-08 02:24:30 +0530

    Stable version

--- App.js
+++ App.js
@@ -7,7 +7,8 @@
  */
 
 import 'react-native-gesture-handler';
-import React from 'react';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -19,8 +20,6 @@ import {
   View,
 } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-
 import {
   Colors,
   DebugInstructions,
@@ -29,66 +28,15 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <NavigationContainer>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
         <Text>Hello WOrld</Text>
       </SafeAreaView>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
