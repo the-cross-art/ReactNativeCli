1415ac7554d6562cd777b61406980395445f35b3
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-07-08 18:42:52 +0530

    Custom Input Text

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
+import Container from '../../components/common/container';
+import Input from '../../components/common/Input';
 
 const Login = () => {
+  const [value, onChangeText] = React.useState('');
   return (
-    <View>
-      <Text>Hi From Login</Text>
-    </View>
+    <Container>
+      <Input
+        label="Username"
+        onChangeText={text => onChangeText(text)}
+        value={value}
+        iconPosition="right"
+        // error={'This field is required'}
+      />
+      <Input
+        label="Password"
+        onChangeText={text => onChangeText(text)}
+        value={value}
+        icon={<Text>HIDE</Text>}
+        iconPosition="right"
+      />
+    </Container>
   );
 };
 
